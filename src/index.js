4b0fdba571ed7548bf9e1b9b30bc39c65b5f2188
soniqmidd <soniqmidd@users.noreply.github.com>
2023-12-01 20:08:35 -0600

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// 3 Display the generated poem
 function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
@@ -11,14 +10,16 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("user-instructions");
-  // 1 build the API URL
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "a04oea55bac37b7e42f762c43b8c6dft";
   let context =
-    "You are a romantic poems expert that loves to write short poems. Your mission is to generate a 4 lined poem in basic HTML and seperate each line wth a <br />. Make sure to follow the user instructions. Do not include a title to the poemFormElement. Sign the poem with 'SheCodes AI' inside a <strong> element at of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 6 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a English poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  // 2 Make a call to the API using axios
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an English poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
